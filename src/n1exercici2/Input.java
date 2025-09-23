41893f16b045cee3c79667ca3edafdeebb98379f
Esther Arias <eariasvalor@gmail.com>
2025-09-23 13:39:51 +0200

    Custom Exception methods added

--- src/n1exercici2/Input.java
+++ src/n1exercici2/Input.java
@@ -77,6 +77,79 @@ public class Input {
         return value;
     }
 
+    public static char readChar(String message) throws CustomException {
+        char value = ' ';
+        String input = "";
+        boolean correct = false;
+
+        while (!correct) {
+            System.out.println(message + ": ");
+            input = scanner.nextLine().trim();
+
+            try {
+                if (input.length() != 1) {
+                    throw new CustomException("Has d'introduir un sol caràcter.");
+                }
+                value = input.charAt(0);
+                correct = true;
+            } catch (CustomException e) {
+                System.out.println(e.getMessage());
+            }
+
+            }
+
+            return value;
+        }
+
+    public static String readString(String message) throws CustomException {
+        String value = "";
+        String input = "";
+        boolean correct = false;
+
+        while(!correct) {
+            System.out.println(message + ": ");
+            input = scanner.nextLine();
+
+            try {
+                if(input.isEmpty()) {
+                    throw new CustomException("La teva resposta no pot estar buida");
+                }
+                value = scanner.nextLine();
+                correct = true;
+            } catch (CustomException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return value;
+
+    }
+
+    public static boolean readYesNo(String message) throws CustomException {
+        boolean value = false;
+        boolean correct = false;
+        String input = "";
+        char answer = ' ';
+
+        while (!correct) {
+            System.out.println(message + ": ");
+            input = scanner.nextLine().trim();
+
+            try {
+                if (input.length() != 1) {
+                    throw new CustomException("Has d'introduir només s o n.");
+                }
+                answer = input.charAt(0);
+                value = (answer == 's') ? true : false;
+                correct = true;
+            } catch (CustomException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return value;
+    }
+
+    }
 
 
 }
